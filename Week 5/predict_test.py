7660eb61367b9ec697048ea096a0df009c8fe509
Nikhil_Shrestha <n.shrestha17@gmail.com>
2021-10-11 17:49:54 +0530

    Adding week 5 homework

---
+++ Week 5/predict_test.py
@@ -0,0 +1,22 @@
+
+import requests
+
+
+url = 'http://localhost:9696/predict'
+
+customer_id = 'xyz-123'
+customer = {"contract": "two_year", "tenure": 1, "monthlycharges": 10}
+
+
+response = requests.post(url, json=customer).json()
+print(response)
+
+if response['churn'] == True:
+    print('sending promo email to %s' % customer_id)
+else:
+    print('not sending promo email to %s' % customer_id)
+
+
+
+
+
